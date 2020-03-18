6fc3eccee92f3c3436834995463a65e338807fb2
Daniel R <lom-pm-c@hotmail.com>
2020-03-18 13:20:28 -0500

    chore: moved app structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from './pages/AppPage';
+import { App } from './App';
 
 if (typeof window !== 'undefined'){
     ReactDOM.render(
